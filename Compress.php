ccd19c56b6c0a7d9737d617a7aac82121555f29a
Ian <mypetertw@gmail.com>
2019-10-18 16:06:50 +0800

    NEW

---
+++ Compress.php
@@ -0,0 +1,56 @@
+<?php
+
+function UPLOAD($FIELD, $ID, $LOCATION) {
+
+    $type = strtolower(strrchr($_FILES[''.$FIELD.'']['name'], '.'));
+    $imgSource = $SYSTEM_VARIABLE['UPLOAD_ROOT_DIR'] . $LOCATION . $ID . $type;
+    if (move_uploaded_file($_FILES[''.$FIELD.'']['tmp_name'], $imgSource)) {
+      return $SYSTEM_VARIABLE['HOST_URL'] . $LOCATION . $ID . $type;
+    } else {
+      return false;
+    }
+  }
+
+function COMPRESS_SIZE($FIELD, $ID, $LOCATION, $SOURSE_LOCATION, $WIDTH_LIMIT) {
+
+    $type = strtolower(strrchr($_FILES[''.$FIELD.'']['name'], '.'));
+    $imgSource = $SYSTEM_VARIABLE['UPLOAD_ROOT_DIR'] . $SOURSE_LOCATION . $ID . $type;
+    $imgReader = $SYSTEM_VARIABLE['UPLOAD_ROOT_DIR'] . $LOCATION . $ID . $type;
+    list($width, $height, $type_no) = getimagesize($imgSource);
+
+    if ($width > $WIDTH_LIMIT) {
+      $ratio = $width / $height;
+      if ($ratio > 1) {
+        $newWidth = $WIDTH_LIMIT;
+        $newHeight = $WIDTH_LIMIT / $ratio;
+      } else {
+        $newWidth = $WIDTH_LIMIT * $ratio;
+        $newHeight = $WIDTH_LIMIT;
+      }
+    } else {
+      $newWidth = $width;
+      $newHeight = $height;
+    }
+
+    switch ($type_no) {
+      case 2:
+        header('Content-Type:image/jpeg');
+        $imageWp = imagecreatetruecolor($newWidth, $newHeight);
+        $image = imagecreatefromjpeg($imgSource);
+        imagecopyresampled($imageWp, $image, 0, 0, 0, 0, $newWidth, $newHeight, $width, $height);
+        imagejpeg($imageWp, $imgReader, 100);
+        imagedestroy($imageWp);
+      break;
+
+      case 3:
+        header('Content-Type:image/png');
+        $imageWp = imagecreatetruecolor($newWidth, $newHeight);
+        $image = imagecreatefrompng($imgSource);
+        imagecopyresampled($imageWp, $image, 0, 0, 0, 0, $newWidth, $newHeight, $width, $height);
+        imagejpeg($imageWp, $imgReader, 100);
+        imagedestroy($imageWp);
+      break;
+    }
+
+    return $SYSTEM_VARIABLE['HOST_URL'] . $LOCATION . $ID . $type;
+ }
---
+++ README.md
@@ -0,0 +1,25 @@
+# gd-image-upload
+### 
+
+---
+
+## Getting Started
+
+Put `pull.php` to your `/` of project.
+
+## Setting
+
+Setting your Bitbucket information.
+
+```php
+# SETTING
+define('BITBUCKET_NAME', '');
+define('GIT_PASSWORD', '');
+define('REPOSITORY_NAME', '');
+```
+
+## Usage
+
+```sh
+$ php pull.php
+```
